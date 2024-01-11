dd8426efc83a26535915173c817c10472dba6604
ruby_1 <bakuteria7125@icloud.com>
2024-01-12 06:31:37 +0900

    通常ログインの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :success, :danger
+
+  protected
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    login_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   root "static_pages#top"
+
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks'
+  }
+
+  devise_scope :user do
+    get 'login', to: 'users/sessions#new'
+    post 'login', to: 'users/sessions#create'
+    get 'sign_up', to: 'users/registrations#new'
+    delete 'logout', to: 'users/sessions#destroy'
+  end
 end
