96b054191113b682aea87183a744ab549258b627
ruby_1 <bakuteria7125@icloud.com>
2024-01-30 19:15:58 +0900

    マイグレの変更

--- db/migrate/20240122110831_remove_items_count_from_sub_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveItemsCountFromSubUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :sub_users, :items_count, :integer
-  end
-end
--- db/migrate/20240122111031_add_items_count_to_sub_users.rb
+++
@@ -1,9 +0,0 @@
-class AddItemsCountToSubUsers < ActiveRecord::Migration[7.0]
-  def self.up
-    add_column :sub_users, :items_count, :integer, null: false, default: 0
-  end
-
-  def self.down
-    remove_column :sub_users, :items_count
-  end
-end
