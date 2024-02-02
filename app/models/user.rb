2a95b7fdb26bdc0a8399e0c636537ef53fec90a8
ruby_1 <bakuteria7125@icloud.com>
2024-02-02 19:50:58 +0900

    コメント

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable,
+         :recoverable, :rememberable, :validatable, :trackable, :confirmable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
          validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
